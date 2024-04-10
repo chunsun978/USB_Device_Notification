3c8140830f2ff8f229fae9714396e8087a673275
Chun Sun <chunsun978@gmail.com>
2024-04-09 23:21:17 -0400

    Update udev.cpp

--- src/udev.cpp
+++ src/udev.cpp
@@ -13,8 +13,8 @@ struct udev_device *dev;
 
 udev = udev_new();
 if (!udev) {
-	printf("Can't create udev\n");
-	exit(1);
+    printf("Can't create udev\n");
+    exit(1);
 }
 
 // Get a handle to the device 
@@ -35,81 +35,80 @@ udev_unref(udev);
 
 namespace usb {
 class Device {
-	struct udev_device *dev;
+    struct udev_device *dev;
 	
-	public:
-		Device() = delete;
-		Device(struct udev_device* dev) : dev(dev) {}
+    public:
+	Device() = delete;
+	Device(struct udev_device* dev) : dev(dev) {}
 
-		Device(const Device& other) : dev(other.dev) {
-			udev_device_ref(dev);
-		}
+	Device(const Device& other) : dev(other.dev) {
+	    udev_device_ref(dev);
+	}
 
-		~Device() {
-        udev_device_unref(dev);
-		}
+	~Device() {
+            udev_device_unref(dev);
+	}
 
-		std::string action() const { return udev_device_get_action(dev); }
+	std::string action() const { return udev_device_get_action(dev); }
 
-		std::string attr(const char* name) const {
-			const char* val = udev_device_get_sysattr_value(dev, name);
-			return val ? val : "";
-		}
+	std::string attr(const char* name) const {
+	    const char* val = udev_device_get_sysattr_value(dev, name);
+	    return val ? val : "";
+	}
 };
 
 class Monitor {
-	struct udev_monitor *mon;
-
-	public:
-		Monitor() {
+    struct udev_monitor *mon;
+     public:
+	Monitor() {
     	struct udev* udev = udev_new();
-      mon = udev_monitor_new_from_netlink(udev, "udev");
+        mon = udev_monitor_new_from_netlink(udev, "udev");
 			udev_monitor_filter_add_match_subsystem_devtype(mon, "usb", NULL);
 			udev_monitor_enable_receiving(mon);
+	}
+
+	Monitor(const Monitor& other) = delete;
+
+	~Monitor() {
+	    udev_monitor_unref(mon);
+	}
+
+	Device wait(std::function<bool(const Device&)> process) {
+	    struct pollfd fds[1];
+	    fds[0].events = POLLIN;
+	    fds[0].fd = udev_monitor_get_fd(mon);
+
+	    while (true) {
+	        int ret = poll(fds, 1, -1);
+	        if (ret < 0) {
+		    throw std::system_error(errno, std::system_category(),
+		        "Poll failed");
 		}
-
-		Monitor(const Monitor& other) = delete;
-
-		~Monitor() {
-			udev_monitor_unref(mon);
+		if (ret) {
+		    Device d(udev_monitor_receive_device(mon));
+		    // call process() to check if the device is matched.
+		    // In this case, it will check if the USB ID is matched.
+		    if (process(d)) {
+		        return d;
+		    };
 		}
-
-		Device wait(std::function<bool(const Device&)> process) {
-			struct pollfd fds[1];
-			fds[0].events = POLLIN;
-			fds[0].fd = udev_monitor_get_fd(mon);
-
-			while (true) {
-					int ret = poll(fds, 1, -1);
-					if (ret < 0) {
-						throw std::system_error(errno, std::system_category(),
-								"Poll failed");
-					}
-					if (ret) {
-						Device d(udev_monitor_receive_device(mon));
-						// call process() to check if the device is matched.
-						// In this case, it will check if the USB ID is matched.
-						if (process(d)) {
-							return d;
-						};
-					}
-			}
-		}
+	    }
+      }
 };
 };
 
 int main() {
-	usb::Monitor mon;
-	usb::Device d = mon.wait([](auto& d) { 
-		// this is a lambda function to check if the USB ID is matched.
-		// if matched, it will exit and display pwer usage.
-		// if not, the loop will continue.
-		auto id = d.attr("idVendor") + ":" + d.attr("idProduct");
-		auto produce = d.attr("product");
-		std::cout << "Check [" << id << "] action: " << d.action() << std::endl;
-		return d.action() == "bind" && id == "1f75:0918";
-	});
-	std::cout << d.attr("product") << " connected, uses up to "
-						<< d.attr("bMaxPower") << std::endl;
-	return 0;
+    usb::Monitor mon;
+    usb::Device d = mon.wait([](auto& d) { 
+	// this is a lambda function to check if the USB ID is matched.
+	// if matched, it will exit and display pwer usage.
+	// if not, the loop will continue.
+	auto id = d.attr("idVendor") + ":" + d.attr("idProduct");
+	auto produce = d.attr("product");
+	std::cout << "Check [" << id << "] action: " << d.action() << std::endl;
+	return d.action() == "bind" && id == "1f75:0918";
+    });
+    std::cout << d.attr("product") << " connected, uses up to "
+				<< d.attr("bMaxPower") << std::endl;
+    return 0;
 }
